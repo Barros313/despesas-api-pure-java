8b16cf4a49082742c4d1d1652bdd3f865ffed40a
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-09-06 10:54:40 -0300

    Added get integer function
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/utility/input/GetInput.java
+++ src/main/java/utility/input/GetInput.java
@@ -1,2 +1,28 @@
-package utility.input;public class getInteger {
+package utility.input;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GetInput {
+    public int integer(String message) {
+        Scanner scan = new Scanner(System.in);
+        int number = 0;
+        boolean valid = false;
+
+        while (!valid) {
+            try {
+                valid = true;
+                System.out.print(message);
+                number = scan.nextInt();
+            } catch (InputMismatchException ime) {
+                System.err.println("Invalid input");
+                valid = false;
+                scan.next();
+            }
+        }
+
+        scan.close();
+
+        return number;
+    }
 }
